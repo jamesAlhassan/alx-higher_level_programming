4ff4ec410a811a530428798f0aa7e0d3c1749a98
James Alhassan <jamesalhassan052@gmail.com>
2022-11-29 18:26:08 +0000

    3-print_alphabt.py updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for letter in range(ord("a"), ord("z") + 1):
-    if letter != ord("q") or letter != ord("e"):
+    if letter == ord("q") or letter == ord("e"):
+        continue
+    else:
         print("{0:c}".format(letter), end="")

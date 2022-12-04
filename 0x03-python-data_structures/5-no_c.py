91b138339b1e9d064e216c265fd9a926990b6b38
James Alhassan <jamesalhassan052@gmail.com>
2022-12-04 15:30:12 +0000

    5-no_c.py added

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhon3
+
+
+def no_c(my_string):
+    new_str = ""
+    for letter in my_string:
+        if letter != "C" and letter != "c":
+            new_str += letter
+    return new_str

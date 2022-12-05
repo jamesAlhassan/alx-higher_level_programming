2a5f7ff22b44acb88200fd54bb005c664044f701
James Alhassan <jamesalhassan052@gmail.com>
2022-12-05 13:23:25 +0000

    9-main.py added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@
 def max_integer(my_list=[]):
     largest = None
     for num in my_list:
-        if num is None or num > largest:
+        if num == None or num > largest:
             largest = num

ca2cf93ce2e0f256d3fdcd9e3e9ad5cc43637cc2
James Alhassan <jamesalhassan052@gmail.com>
2022-12-20 18:17:01 +0000

    1-square.py updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@
 class Square:
     """a class 'Square' with a private attribute 'size' """
     def __init__(self, size):
-        self._size = size
-
-
-square = Square(size)
+        """ instance initialization
+        Arg: size 
+        """
+        self.__size = size

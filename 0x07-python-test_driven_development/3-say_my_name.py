40ce5599fb3036b57047db5a1cc35ad2c0ccec90
James Alhassan <jamesalhassan052@gmail.com>
2023-01-01 15:32:16 +0000

    3-say_my_name.py added

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+A function that prints first and last name
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints first_name and last_name
+
+    Args:
+        first_name (str): The fisrt name
+        last_name (str): The last name
+
+    Raises:
+        TypeError: If either the first_name and last_name are not strings
+
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")

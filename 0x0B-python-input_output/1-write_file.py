de4577f35bd7c358aff6afeb2beb70a5f58cc579
James Alhassan <jamesalhassan052@gmail.com>
2023-01-11 05:17:45 +0000

    1-write_file.py added

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file"""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)

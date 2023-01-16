31313bfb401ec80dd526941af298a28bcc01cc7c
James Alhassan <jamesalhassan052@gmail.com>
2023-01-16 20:40:13 +0000

    rectangle.py updated:display(self) func added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,5 +84,11 @@ class Rectangle(Base):
 
     def display(self):
         """Displays '#' shape of the rectangle"""
-        for _ in range(self.width):
-            print('')
+        for y in range(self.y):
+            print("")
+        for row in range(self.__height):
+            for x in range(self.x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#", end="")
+            print()

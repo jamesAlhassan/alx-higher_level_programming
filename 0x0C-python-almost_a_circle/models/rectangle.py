d716c7ca9485c0002da3e58b7c1857c55539829f
James Alhassan <jamesalhassan052@gmail.com>
2023-01-16 15:56:59 +0000

    rectangle.py, getters added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,23 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """Get the value of width like an attribute"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the value of height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """ Get the value of x"""
+        return self.__x
+
+    @property
+    def (self):
+        """Get the value of y like an attribute"""
+        return self.__y

1f686ae0d97f8bb7fba24fabe71555dab75e17ef
James Alhassan <jamesalhassan052@gmail.com>
2023-01-16 15:33:55 +0000

    rectangle.py  updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """A child class of Base """
 
+from model.base import Base
+
 
 class Rectangle(Base):
     """A class than inherits from Base """
-
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Object Instantiation method"""
         self.width = width

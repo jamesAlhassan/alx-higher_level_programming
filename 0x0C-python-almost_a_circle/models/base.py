cd24c2c7a348d11647e5ca186d50033809bdb798
James Alhassan <jamesalhassan052@gmail.com>
2023-01-17 05:37:12 +0000

    base.py updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ that returns the JSON string representation of list_dictionaries"""
-         if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
+
         if (type(list_dictionaries) != list or not
                 all(type(i) == dict for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")

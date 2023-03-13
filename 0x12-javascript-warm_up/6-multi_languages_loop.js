61933611cc30e4093d2dfb3902fae2bdc39cfe4d
James Alhassan <jamesalhassan052@gmail.com>
2023-03-13 22:32:16 +0000

    Add 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 
 for (i = 0; i < arr.length; i++) {

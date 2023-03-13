c0a461434590965fa2906431f555b8f7cac9c4a7
James Alhassan <jamesalhassan052@gmail.com>
2023-03-13 21:40:32 +0000

    Add 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argc = process.argv.length;
+
+if (argc < 1) {
+  console.log('No argument');
+} else if (argc === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}

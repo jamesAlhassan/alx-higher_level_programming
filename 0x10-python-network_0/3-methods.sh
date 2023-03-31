eb0fce0223bcc2883bae805f8a55fc5899698392
James Alhassan <jamesalhassan052@gmail.com>
2023-03-31 05:58:21 +0000

    Add 100-status_code

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# display all HTTP methods the server will accept
+# takes in a URL and displays all HTTP methods the server will accept.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
-

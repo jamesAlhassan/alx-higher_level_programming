0a9c0c5b956418da2fe8d0d821ae9d5835ee0301
James Alhassan <jamesalhassan052@gmail.com>
2023-03-31 22:37:43 +0000

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
-import urllib.request
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as ur
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
+    request = ur.Request("https://alx-intranet.hbtn.io/status")
+    with ur.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))

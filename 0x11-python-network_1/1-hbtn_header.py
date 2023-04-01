b40da9de829209780d472cf2b8e6d885aee614aa
James Alhassan <jamesalhassan052@gmail.com>
2023-04-01 04:43:09 +0000

    Add 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""sends request & displays the value of the X-Request-Id variable """
+import urllib.request as ur
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    request = ur.Request(url)
+
+    with ur.urlopen(request) as res:
+        print(dict(res.headers).get('X-Request-I'))

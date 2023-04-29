5489255b86264548dd9b6574852e7a532bdbc460
James Alhassan <jamesalhassan052@gmail.com>
2023-04-29 13:23:26 +0000

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,3 +5,30 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi.dev/api/films/${id}/`;
 let characters = [];
+
+request(url, (err, res, content) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const data = JSON.parse(content);
+  characters = data.characters;
+  getChar(0);
+});
+
+const getChar = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (err, res, content) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const characterData = JSON.parse(content);
+    console.log(characterData.name);
+    getChar(index + 1);
+  });
+};
